4731c1917431b04ae1a8aece861e34a4869823b3
Omar Khan <omakhan98@gmail.com>
2023-05-31 02:41:31 +0500

    added side dropdown nav that appears after scrolling

--- src/Components/IntroText/IntroText.js
+++ src/Components/IntroText/IntroText.js
@@ -1,28 +1,23 @@
 import React, { useEffect } from "react";
-import { useTransition, animated } from "react-spring";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./IntroText.scss";
 
 const IntroText = () => {
-  const slideInLeftAnimation = useTransition(true, {
-    from: { opacity: 0, transform: "translateX(-100%)" },
-    enter: { opacity: 1, transform: "translateX(0%)" },
-    config: { duration: 2500 },
-  });
+  useEffect(() => {
+    AOS.init({ duration: 600 });
+  }, []);
 
   return (
     <div className="intro-text">
-      {slideInLeftAnimation((styles, item) =>
-        item ? (
-          <animated.div style={styles}>
-            <h1 className="first-line">
-              Hi, Im <span className="aqua-text">Omar</span>
-            </h1>
-            <h1 className="second-line">
-              <span className="gradient-text">I'm a frontend developer.</span>
-            </h1>
-          </animated.div>
-        ) : null
-      )}
+      <div className="fade-in-left" data-aos="fade-right">
+        <h1 className="first-line">
+          Hi, Im <span className="aqua-text">Omar</span>
+        </h1>
+        <h1 className="second-line">
+          <span className="gradient-text">I'm a frontend developer.</span>
+        </h1>
+      </div>
     </div>
   );
 };
